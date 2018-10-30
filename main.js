109700edc7207e5942ca9678a580e8282ef8928b
Alex McDaniel <amcdaniel186@gmail.com>
2018-10-30 07:01:52 -0400

    2nd-update

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("My name is Bob, and I'm writing JavaScript");
+
+let nay, age, email;
+
+let name = "ironhacker";
+let ag;
+age = 45;
+console.log(name);
+console.log(age);
+
+let favoriteFood;
+console.log(favoriteFood);
